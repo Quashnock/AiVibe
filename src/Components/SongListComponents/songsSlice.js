f72028523afecc9250618745b63e300abe226f84
Quashnock <awq@uw.edu>
2024-12-20 15:31:40 -0800

    Fixed song fetch API endpoint

--- src/Components/SongListComponents/songsSlice.js
+++ src/Components/SongListComponents/songsSlice.js
@@ -7,11 +7,8 @@ export const getSongs = createAsyncThunk("songs/getSongs", async (songs) => {
   const songSet = new Set();
   return Promise.all(
     songs.map(async (song) => {
-      const track = encodeURIComponent(song[0]);
-      const artist = encodeURIComponent(song[1]);
-      const query = `track:${track}artist:${artist}`;
       const response = await fetch(
-        `https://api.spotify.com/v1/search?q=${query}&type=track&limit=1&offset=0`,
+        `https://api.spotify.com/v1/search?q=q%3Dremaster%252520track%253A${song[0]}%252520artist%253A${song[1]}&type=track`,
         {
           method: "GET",
           headers: {
@@ -21,6 +18,7 @@ export const getSongs = createAsyncThunk("songs/getSongs", async (songs) => {
         }
       );
       const songResult = await response.json();
+      console.log(songResult);
       if (!songSet.has(songResult.tracks.items[0].id)) {
         songSet.add(songResult.tracks.items[0].id);
         return songResult;
